140e016db8d5caecf9381ec98c04b312ed39bbc2
Alban González <alban.gonzalez@outlook.com>
2024-10-06 15:22:42 -0600

    API users list

---
+++ app/api/v1/users/route.ts
@@ -0,0 +1,20 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+
+export async function GET() {
+  await dbConnect();
+
+  try {
+    const users = await User.find();
+
+    if (!users) {
+      return Response.json({ success: false }, { status: 404 });
+    }
+
+    const r = users.map((user) => user.toJSON());
+
+    return Response.json({ success: true, data: r });
+  } catch (e) {
+    return Response.json({ success: false }, { status: 500 });
+  }
+}
