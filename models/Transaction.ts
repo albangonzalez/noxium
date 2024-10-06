33b7fd88ddeded1454c67a370aab26ce3867f79b
Alban González <alban.gonzalez@outlook.com>
2024-10-06 15:45:49 -0600

    Transaction model

---
+++ models/Transaction.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+export interface Transaction {
+  label: string;
+  amount: number;
+  date: Date;
+  madeBy: [
+    {
+      amount: number | null;
+      user: string;
+      percent: number | null;
+    },
+  ];
+}
+
+const schema = new mongoose.Schema({
+  label: String,
+  amount: Number,
+  date: Date,
+  madeBy: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      amount: Number,
+      percent: Number,
+    },
+  ],
+});
+
+schema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+
+export default mongoose.models.Transaction ||
+  mongoose.model<Transaction>("Transaction", schema);
