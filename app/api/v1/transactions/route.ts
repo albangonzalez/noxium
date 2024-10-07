6192a55f6b8db097d82294264bec4641221791d4
Alban González <alban.gonzalez@outlook.com>
2024-10-07 13:15:16 -0600

    List transactions by day API

---
+++ app/api/v1/transactions/route.ts
@@ -0,0 +1,43 @@
+import dbConnect from "@/lib/dbConnect";
+import Transaction from "@/models/Transaction";
+
+export async function GET() {
+  await dbConnect();
+
+  try {
+    const transactions = await Transaction.aggregate()
+      .sort({
+        date: -1,
+      })
+      .addFields({
+        dateOnly: { $dateToString: { format: "%Y-%m-%d", date: "$date", timezone: "-05" } }
+      })
+      .lookup({
+        from: "users",
+        localField: "madeBy.user",
+        foreignField: "_id",
+        as: "user",
+      })
+      .project({
+        madeBy: 0,
+        createdAt:0,
+        updatedAt: 0,
+      })
+      .group({
+        _id: "$dateOnly",
+        items: { $push: "$$ROOT" }
+      })
+      .sort({
+        _id: -1
+      })
+      .exec();
+
+    if (!transactions) {
+      return Response.json({ success: false }, { status: 404 });
+    }
+
+    return Response.json({ success: true, data: transactions });
+  } catch (e) {
+    return Response.json({ success: false }, { status: 500 });
+  }
+}
